0d38033a84c22d986ffcb2511af1d15d3ad80fe3
akshayvernekar <vernekarakshay@yahoo.com>
2019-01-17 08:56:28 +0800

    more changes

--- HX711.c
+++ HX711.c
@@ -1,10 +1,18 @@
 #include "HX711.h"
+#include "esp_log.h"
+#include <rom/ets_sys.h>
 
-HX711_init(char dout, char pd_sck, char gain)
+#define HIGH 1
+#define LOW 0
+
+static const char* DEBUGTAG = "HX711";
+
+void HX711_init(gpio_num_t dout, gpio_num_t pd_sck, char gain)
 {
 
 	GPIO_PD_SCK = pd_sck;
 	GPIO_DOUT = dout;
+	GAIN = gain;
 
 	gpio_config_t io_conf;
     io_conf.intr_type = GPIO_PIN_INTR_DISABLE;
@@ -19,6 +27,9 @@ HX711_init(char dout, char pd_sck, char gain)
     io_conf.mode = GPIO_MODE_INPUT;
     io_conf.pull_up_en = 1;
 	gpio_config(&io_conf);
+
+	void HX711_powerDown();
+	void HX711_powerUp();
 	
 }
 
@@ -27,41 +38,99 @@ bool HX711_isReady()
 	return gpio_get_level(GPIO_DOUT);
 }
 
-void set_gain(char gain)
+void HX711_setGain(char gain)
 {
-
+	GAIN = gain;
 }
 
-long read()
+uint8_t HX711_shiftIn()
 {
-	while (!is_ready()) 
+	uint8_t value = 0;
+
+    for(int i = 0; i < 8; ++i) 
+    {
+        gpio_set_level(GPIO_PD_SCK, HIGH);
+        ets_delay_us(5);
+        value |= gpio_get_level(GPIO_DOUT) << (7 - i); //get level returns 
+        gpio_set_level(GPIO_PD_SCK, LOW);
+        ets_delay_us(5);
+    }
+
+    return value;
+}
+
+long HX711_readWeight()
+{
+	while (!HX711_isReady()) 
 	{
-		// Will do nothing on Arduino but prevent resets of ESP8266 (Watchdog Issue)
-		vTaskDelay(20 / portTICK_PERIOD_MS);
+		vTaskDelay(1/ portTICK_PERIOD_MS);
 	}
+
+
 	unsigned long value = 0;
 	uint8_t data[3] = { 0 };
 	uint8_t filler = 0x00;
 
+	// pulse the clock pin 24 times to read the data
+	data[2] = HX711_shiftIn();
+	data[1] = HX711_shiftIn();
+	data[0] = HX711_shiftIn();
+
+	// set the channel and the gain factor for the next reading using the clock pin
+	for (unsigned int i = 0; i < GAIN; i++) 
+	{
+		gpio_set_level(GPIO_PD_SCK, HIGH);
+		ets_delay_us(5);
+		gpio_set_level(GPIO_PD_SCK, LOW);
+		ets_delay_us(5);
+	}
+
+	// Replicate the most significant bit to pad out a 32-bit signed integer
+	if (data[2] & 0x80) {
+		filler = 0xFF;
+	} else {
+		filler = 0x00;
+	}
+
+	// Construct a 32-bit signed integer
+	value = ( (unsigned long)(filler) << 24
+			| (unsigned long)(data[2]) << 16
+			| (unsigned long)(data[1]) << 8
+			| (unsigned long)(data[0]) );
+
+return (long)(value);
 
 }
 
-long read_average(char times)
+long HX711_readAverage(char times)
 {
-
+	ESP_LOGI(DEBUGTAG, "===================== READ AVERAGE START ====================");
+	long sum = 0;
+	long value ;
+	for (char i = 0; i < times; i++) 
+	{
+		value = 0;
+		value = HX711_readWeight();
+		ESP_LOGI(DEBUGTAG, "Itr %d , value =%ld",i,value);
+		sum += value;
+		vTaskDelay(20/ portTICK_PERIOD_MS);
+	}
+	ESP_LOGI(DEBUGTAG, "===================== READ AVERAGE END : %ld ====================",(sum / times));
+	return sum / times;
 }
 
-double get_value(char times = 1)
+double HX711_getValue(char times)
 {
 
+	return 0;
 }
 
-void power_down()
+void HX711_powerDown()
 {
- 	gpio_set_level(GPIO_PD_SCK, 1);
+ 	gpio_set_level(GPIO_PD_SCK, HIGH);
 }
 
-void power_up()
+void HX711_powerUp()
 {
- 	gpio_set_level(GPIO_PD_SCK, 0);
+ 	gpio_set_level(GPIO_PD_SCK, LOW);
 }
--- HX711.h
+++ HX711.h
@@ -1,15 +1,24 @@
 #ifndef HX711_h
 #define HX711_h
 
-#define GAIN_128 128
-#define GAIN_64	 64
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "freertos/event_groups.h"
+#include "driver/gpio.h"
+
+#define GAIN_128 1
+#define GAIN_64	 2
 
 //clock GPIO
-static char GPIO_PD_SCK = GPIO_NUM_10;
-static char GPIO_DOUT = GPIO_NUM_11;
+static gpio_num_t GPIO_PD_SCK = GPIO_NUM_10;
+static gpio_num_t GPIO_DOUT = GPIO_NUM_11;
 static char GAIN = GAIN_128;
 
-HX711_init(char dout, char pd_sck, char gain);
+void HX711_init(gpio_num_t dout, gpio_num_t pd_sck, char gain);
 
 // check if HX711 is ready
 // from the datasheet: When output data is not ready for retrieval, digital output pin DOUT is high. Serial clock
@@ -19,22 +28,22 @@ bool HX711_isReady();
 // set the gain factor; takes effect only after a call to read()
 // channel A can be set for a 128 or 64 gain; channel B has a fixed 32 gain
 // depending on the parameter, the channel is also set to either A or B
-void set_gain(char gain = 128);
+void HX711_setGain(char gain );
 
 // waits for the chip to be ready and returns a reading
-long read();
+long HX711_readWeight();
 
 // returns an average reading; times = how many times to read
-long read_average(char times = 10);
+long HX711_readAverage(char times);
 
 
 // returns (read_average() - OFFSET), that is the current value without the tare weight; times = how many readings to do
-double get_value(char times = 1);
+double HX711_getValue(char times );
 
 // puts the chip into power down mode
-void power_down();
+void HX711_powerDown();
 
 // wakes up the chip after power down mode
-void power_up();
+void HX711_powerUp();
 
 #endif
