12afa41b83bee4570911e8f1c2906b28e1596a7e
akshayvernekar <vernekarakshay@yahoo.com>
2019-01-24 10:20:21 +0800

    merging changes from private branch

--- HX711.c
+++ HX711.c
@@ -4,15 +4,20 @@
 
 #define HIGH 1
 #define LOW 0
+#define CLOCK_DELAY_US 20
 
-static const char* DEBUGTAG = "HX711";
+#define DEBUGTAG "HX711"
 
-void HX711_init(gpio_num_t dout, gpio_num_t pd_sck, char gain)
+static gpio_num_t GPIO_PD_SCK = GPIO_NUM_15;	// Power Down and Serial Clock Input Pin
+static gpio_num_t GPIO_DOUT = GPIO_NUM_14;		// Serial Data Output Pin
+static HX711_GAIN GAIN = eGAIN_128;		// amplification factor
+static unsigned long OFFSET = 0;	// used for tare weight
+static float SCALE = 1;	// used to return weight in grams, kg, ounces, whatever
+
+void HX711_init(gpio_num_t dout, gpio_num_t pd_sck, HX711_GAIN gain )
 {
-
 	GPIO_PD_SCK = pd_sck;
 	GPIO_DOUT = dout;
-	GAIN = gain;
 
 	gpio_config_t io_conf;
     io_conf.intr_type = GPIO_PIN_INTR_DISABLE;
@@ -25,22 +30,22 @@ void HX711_init(gpio_num_t dout, gpio_num_t pd_sck, char gain)
     io_conf.intr_type = GPIO_PIN_INTR_DISABLE;
     io_conf.pin_bit_mask = (1UL<<GPIO_DOUT);
     io_conf.mode = GPIO_MODE_INPUT;
-    io_conf.pull_up_en = 1;
+    io_conf.pull_up_en = 0;
 	gpio_config(&io_conf);
 
-	void HX711_powerDown();
-	void HX711_powerUp();
-	
+	HX711_set_gain(gain);
 }
 
-bool HX711_isReady()
+bool HX711_is_ready()
 {
 	return gpio_get_level(GPIO_DOUT);
 }
 
-void HX711_setGain(char gain)
+void HX711_set_gain(HX711_GAIN gain)
 {
 	GAIN = gain;
+	gpio_set_level(GPIO_PD_SCK, LOW);
+	HX711_read();
 }
 
 uint8_t HX711_shiftIn()
@@ -50,87 +55,123 @@ uint8_t HX711_shiftIn()
     for(int i = 0; i < 8; ++i) 
     {
         gpio_set_level(GPIO_PD_SCK, HIGH);
-        ets_delay_us(5);
+        ets_delay_us(CLOCK_DELAY_US);
         value |= gpio_get_level(GPIO_DOUT) << (7 - i); //get level returns 
         gpio_set_level(GPIO_PD_SCK, LOW);
-        ets_delay_us(5);
+        ets_delay_us(CLOCK_DELAY_US);
     }
 
     return value;
 }
 
-long HX711_readWeight()
+unsigned long HX711_read()
 {
-	while (!HX711_isReady()) 
+	gpio_set_level(GPIO_PD_SCK, LOW);
+	// wait for the chip to become ready
+	while (HX711_is_ready()) 
 	{
-		vTaskDelay(1/ portTICK_PERIOD_MS);
+		vTaskDelay(10 / portTICK_PERIOD_MS);
 	}
 
-
 	unsigned long value = 0;
-	uint8_t data[3] = { 0 };
-	uint8_t filler = 0x00;
 
-	// pulse the clock pin 24 times to read the data
-	data[2] = HX711_shiftIn();
-	data[1] = HX711_shiftIn();
-	data[0] = HX711_shiftIn();
+	//--- Enter critical section ----
+	portDISABLE_INTERRUPTS();
+
+	for(int i=0; i < 24 ; i++)
+	{   
+		gpio_set_level(GPIO_PD_SCK, HIGH);
+        ets_delay_us(CLOCK_DELAY_US);
+        value = value << 1;
+        gpio_set_level(GPIO_PD_SCK, LOW);
+        ets_delay_us(CLOCK_DELAY_US);
+
+        if(gpio_get_level(GPIO_DOUT))
+        	value++;
+	}
 
 	// set the channel and the gain factor for the next reading using the clock pin
 	for (unsigned int i = 0; i < GAIN; i++) 
-	{
+	{	
 		gpio_set_level(GPIO_PD_SCK, HIGH);
-		ets_delay_us(5);
+		ets_delay_us(CLOCK_DELAY_US);
 		gpio_set_level(GPIO_PD_SCK, LOW);
-		ets_delay_us(5);
-	}
+		ets_delay_us(CLOCK_DELAY_US);
+	}	
+	portENABLE_INTERRUPTS();
+	//--- Exit critical section ----
 
-	// Replicate the most significant bit to pad out a 32-bit signed integer
-	if (data[2] & 0x80) {
-		filler = 0xFF;
-	} else {
-		filler = 0x00;
-	}
+	value =value^0x800000;
 
-	// Construct a 32-bit signed integer
-	value = ( (unsigned long)(filler) << 24
-			| (unsigned long)(data[2]) << 16
-			| (unsigned long)(data[1]) << 8
-			| (unsigned long)(data[0]) );
-
-return (long)(value);
-
+	return (value);
 }
 
-long HX711_readAverage(char times)
+
+
+unsigned long  HX711_read_average(char times) 
 {
 	ESP_LOGI(DEBUGTAG, "===================== READ AVERAGE START ====================");
-	long sum = 0;
-	long value ;
+	unsigned long sum = 0;
 	for (char i = 0; i < times; i++) 
 	{
-		value = 0;
-		value = HX711_readWeight();
-		ESP_LOGI(DEBUGTAG, "Itr %d , value =%ld",i,value);
-		sum += value;
-		vTaskDelay(20/ portTICK_PERIOD_MS);
+		sum += HX711_read();
 	}
 	ESP_LOGI(DEBUGTAG, "===================== READ AVERAGE END : %ld ====================",(sum / times));
 	return sum / times;
 }
 
-double HX711_getValue(char times)
+unsigned long HX711_get_value(char times) 
 {
-
-	return 0;
+	unsigned long avg = HX711_read_average(times);
+	if(avg > OFFSET)
+		return avg - OFFSET;
+	else
+		return 0;
 }
 
-void HX711_powerDown()
+float HX711_get_units(char times) 
 {
- 	gpio_set_level(GPIO_PD_SCK, HIGH);
+	return HX711_get_value(times) / SCALE;
 }
 
-void HX711_powerUp()
+void HX711_tare( ) 
 {
- 	gpio_set_level(GPIO_PD_SCK, LOW);
+	ESP_LOGI(DEBUGTAG, "===================== START TARE ====================");
+	unsigned long sum = 0; 
+	sum = HX711_read_average(20);
+	HX711_set_offset(sum);
+	ESP_LOGI(DEBUGTAG, "===================== END TARE: %ld ====================",sum);
+}
+
+void HX711_set_scale(float scale ) 
+{
+	SCALE = scale;
+}
+
+float HX711_get_scale()
+ {
+	return SCALE;
+}
+
+void HX711_set_offset(unsigned long offset)
+ {
+	OFFSET = offset;
+}
+
+unsigned long HX711_get_offset(unsigned long offset) 
+{
+	return OFFSET;
+}
+
+void HX711_power_down() 
+{
+	gpio_set_level(GPIO_PD_SCK, LOW);
+	ets_delay_us(CLOCK_DELAY_US);
+	gpio_set_level(GPIO_PD_SCK, HIGH);
+	ets_delay_us(CLOCK_DELAY_US);
+}
+
+void HX711_power_up() 
+{
+	gpio_set_level(GPIO_PD_SCK, LOW);
 }
--- HX711.h
+++ HX711.h
@@ -1,3 +1,4 @@
+
 #ifndef HX711_h
 #define HX711_h
 
@@ -10,40 +11,63 @@
 #include "freertos/event_groups.h"
 #include "driver/gpio.h"
 
-#define GAIN_128 1
-#define GAIN_64	 2
 
-//clock GPIO
-static gpio_num_t GPIO_PD_SCK = GPIO_NUM_10;
-static gpio_num_t GPIO_DOUT = GPIO_NUM_11;
-static char GAIN = GAIN_128;
+typedef enum HX711_GAIN
+{
+	eGAIN_128 = 1,
+	eGAIN_64 = 3,
+	eGAIN_32 = 2
+}HX711_GAIN;
 
-void HX711_init(gpio_num_t dout, gpio_num_t pd_sck, char gain);
+
+// define clock and data pin, channel, and gain factor
+// channel selection is made by passing the appropriate gain: 128 or 64 for channel A, 32 for channel B
+// gain: 128 or 64 for channel A; channel B works with 32 gain factor only
+void HX711_init(gpio_num_t dout, gpio_num_t pd_sck, HX711_GAIN gain);
+
 
 // check if HX711 is ready
 // from the datasheet: When output data is not ready for retrieval, digital output pin DOUT is high. Serial clock
 // input PD_SCK should be low. When DOUT goes to low, it indicates data is ready for retrieval.
-bool HX711_isReady();
+bool HX711_is_ready();
 
 // set the gain factor; takes effect only after a call to read()
 // channel A can be set for a 128 or 64 gain; channel B has a fixed 32 gain
 // depending on the parameter, the channel is also set to either A or B
-void HX711_setGain(char gain );
+void HX711_set_gain(HX711_GAIN gain);
 
 // waits for the chip to be ready and returns a reading
-long HX711_readWeight();
+unsigned long HX711_read();
 
 // returns an average reading; times = how many times to read
-long HX711_readAverage(char times);
-
+unsigned long HX711_read_average(char times );
 
 // returns (read_average() - OFFSET), that is the current value without the tare weight; times = how many readings to do
-double HX711_getValue(char times );
+unsigned long HX711_get_value(char times);
+
+// returns get_value() divided by SCALE, that is the raw value divided by a value obtained via calibration
+// times = how many readings to do
+float HX711_get_units(char times);
+
+// set the OFFSET value for tare weight; times = how many times to read the tare value
+void HX711_tare();
+
+// set the SCALE value; this value is used to convert the raw data to "human readable" data (measure units)
+void HX711_set_scale(float scale );
+
+// get the current SCALE
+float HX711_get_scale();
+
+// set OFFSET, the value that's subtracted from the actual reading (tare weight)
+void HX711_set_offset(unsigned long offset);
+
+// get the current OFFSET
+unsigned long HX711_get_offset();
 
 // puts the chip into power down mode
-void HX711_powerDown();
+void HX711_power_down();
 
 // wakes up the chip after power down mode
-void HX711_powerUp();
+void HX711_power_up();
 
 #endif
