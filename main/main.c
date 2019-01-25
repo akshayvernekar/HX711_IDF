5ef9aa68663ef70a20d23c9ce096eaf2ff2ed504
akshayvernekar <vernekarakshay@yahoo.com>
2019-01-25 11:43:31 +0800

    creating project structure

---
+++ main/main.c
@@ -0,0 +1,67 @@
+#include "freertos/FreeRTOS.h"
+#include "esp_wifi.h"
+#include "esp_system.h"
+#include "esp_event.h"
+#include "esp_event_loop.h"
+#include "nvs_flash.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+#include "HX711.h"
+
+#define AVG_SAMPLES   10
+#define GPIO_DATA   GPIO_NUM_2
+#define GPIO_SCLK   GPIO_NUM_16
+
+static const char* TAG = "HX711_TEST";
+
+static void weight_reading_task(void* arg);
+static void initialise_weight_sensor(void);
+
+
+static void weight_reading_task(void* arg)
+{
+    HX711_init(GPIO_DATA,GPIO_SCLK,eGAIN_128); 
+    HX711_tare();
+
+    unsigned long weight =0;
+    while(1)
+    {
+        weight = HX711_get_units(AVG_SAMPLES);
+        ESP_LOGI(TAG, "******* weight = %ld *********\n ", weight);
+        //vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
+
+static void initialise_weight_sensor(void)
+{
+    ESP_LOGI(TAG, "****************** Initialing weight sensor **********");
+    xTaskCreatePinnedToCore(weight_reading_task, "weight_reading_task", 2048, NULL, 1, NULL,0);
+}
+
+esp_err_t event_handler(void *ctx, system_event_t *event)
+{
+    return ESP_OK;
+}
+
+void app_main(void)
+{
+    nvs_flash_init();
+    tcpip_adapter_init();
+    ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
+    ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
+    ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
+    wifi_config_t sta_config = {
+        .sta = {
+            .ssid = CONFIG_ESP_WIFI_SSID,
+            .password = CONFIG_ESP_WIFI_PASSWORD,
+            .bssid_set = false
+        }
+    };
+    ESP_ERROR_CHECK( esp_wifi_set_config(WIFI_IF_STA, &sta_config) );
+    ESP_ERROR_CHECK( esp_wifi_start() );
+    ESP_ERROR_CHECK( esp_wifi_connect() );
+
+}
+
