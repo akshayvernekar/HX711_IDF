bbd2c867bfa523b81dccaaf31ebf3dc257e3ac9a
SONG RUIHONG <13486316672@163.com>
2024-08-02 14:58:51 +0800

    Adapt to IDF5.3 version

--- main/main.c
+++ main/main.c
@@ -1,22 +1,19 @@
 #include "freertos/FreeRTOS.h"
-#include "esp_wifi.h"
 #include "esp_system.h"
 #include "esp_event.h"
-#include "esp_event_loop.h"
 #include "nvs_flash.h"
 #include "driver/gpio.h"
 #include "esp_log.h"
 #include "HX711.h"
 
 #define AVG_SAMPLES   10
-#define GPIO_DATA   GPIO_NUM_2
+#define GPIO_DATA   GPIO_NUM_15
 #define GPIO_SCLK   GPIO_NUM_16
 
 static const char* TAG = "HX711_TEST";
 
 static void weight_reading_task(void* arg);
 static void initialise_weight_sensor(void);
-
 
 static void weight_reading_task(void* arg)
 {
@@ -28,42 +25,19 @@ static void weight_reading_task(void* arg)
     {
         weight = HX711_get_units(AVG_SAMPLES);
         ESP_LOGI(TAG, "******* weight = %ld *********\n ", weight);
-        //vTaskDelay(1000 / portTICK_PERIOD_MS);
+        vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
 
 static void initialise_weight_sensor(void)
 {
     ESP_LOGI(TAG, "****************** Initialing weight sensor **********");
-    xTaskCreatePinnedToCore(weight_reading_task, "weight_reading_task", 2048, NULL, 1, NULL,0);
-}
-
-esp_err_t event_handler(void *ctx, system_event_t *event)
-{
-    return ESP_OK;
+    xTaskCreatePinnedToCore(weight_reading_task, "weight_reading_task", 4096, NULL, 1, NULL,0);
 }
 
 void app_main(void)
 {
     nvs_flash_init();
-    tcpip_adapter_init();
-    ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
-    ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
-    ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
-    wifi_config_t sta_config = {
-        .sta = {
-            .ssid = CONFIG_ESP_WIFI_SSID,
-            .password = CONFIG_ESP_WIFI_PASSWORD,
-            .bssid_set = false
-        }
-    };
-    ESP_ERROR_CHECK( esp_wifi_set_config(WIFI_IF_STA, &sta_config) );
-    ESP_ERROR_CHECK( esp_wifi_start() );
-    ESP_ERROR_CHECK( esp_wifi_connect() );
-
     initialise_weight_sensor();
-
 }
 
